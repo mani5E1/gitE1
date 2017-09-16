f94c6f0f7f7b6f32f530426e21a9e6e393f6f26a
mani5E1 <32007942+mani5E1@users.noreply.github.com>
2017-09-16 12:08:24 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+		Student p = new Student(01,"ram",new Date(1996,01,06),63.568);
+		Student p1 = new Student(02,"raju",new Date(1996,01,06),63.568);
+		Student p2 = new Student(03,"kiran",new Date(1996,01,06),63.568);
+		Student p3 = new Student(04,"thonda",new Date(1996,01,06),63.568);
+
+
+	}
+
+}
